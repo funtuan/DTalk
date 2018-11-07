6ea630229b71a221acaff7804d17b766f62b9ae9
funtuan <hank85627@gmail.com>
2018-11-07 14:25:58 +0800

    cookies Test

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+function getCookie(cname)
+{
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0; i<ca.length; i++) 
+  {
+    var c = ca[i].trim();
+    if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+  }
+  return "";
+}
+
+if(!("userid" in Cookies.get())){
+	Cookies.set("userid" , Math.random().toString(36).substring(2));
+}
+
 var app = new Vue({
 	el: '#app',
 	data: {
@@ -5,7 +21,7 @@ var app = new Vue({
 		chatJson: [{
 			type: "status",
 			className: "PostAuthorStatus",
-			content: "中興大學185 加入聊天室",
+			content: "中興大學183 加入聊天室",
 			time: new Date()
 		},
 		{
